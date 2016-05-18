79a8604201142b3c21c5dc3402848f80624258eb
Qinka <qinka@live.com>
2016-05-18 22:20:31 +0800

    update

---
+++ dindo-test/after-success.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -n "$DOCUMENT" ]; then
+  echo update docuemnt
+  git config --global user.name "travis-auto"
+  git config --global user.email "qinka@live.com"
+  cd ~
+  git clone https://qinka:$GITHUB_API_KEY@github.com/Dingo/DingoRelease.git
+  cd DingoRelease
+  export RELPATH=$(pwd)
+  cp $TRAVIS_BUILD_DIR/*.pdf .
+  git add .
+  git commit -am"$travis-ci $(date)"
+  git push origin master
+  cd ~
+  sudo rm -rf $RELPATH
+  cd $TRAVIS_BUILD_DIR
+else
+  echo update docker
+fi
---
+++ dindo-test/document-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "build Dindo Documents"
+cd document
+xelatex Dindo.tex
+xelatex Dindo.tex
+xelatex Dindo.tex
