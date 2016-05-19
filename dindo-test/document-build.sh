af1054a992ae73e91a852cf872acde4d5d5e10ea
Qinka <qinka@live.com>
2016-05-19 20:24:13 +0800

    update

--- dindo-test/document-build.sh
+++ dindo-test/document-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "build Dindo Documents"
 cd document
+wget https://raw.githubusercontent.com/DingoLab/dingo.tex/master/dingo.cls
+wget https://raw.githubusercontent.com/DingoLab/dingo.tex/master/geometry.sty
+wget https://raw.githubusercontent.com/DingoLab/dingo.tex/master/geometry.cfg
 xelatex Dindo.tex
 xelatex Dindo.tex
 xelatex Dindo.tex
