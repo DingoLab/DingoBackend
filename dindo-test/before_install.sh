85b604c57232c28df0f0ae09ede1d0df34efb9fd
Qinka <qinka@live.com>
2016-05-19 19:53:56 +0800

    update

--- dindo-test/before_install.sh
+++ dindo-test/before_install.sh
@@ -3,12 +3,14 @@ echo "build"
 if [ -n "$DOCUMENT" ]; then
   echo "for document"
   sudo apt-get update
-  sudo apt-get install -y --no-install-recommeds wget texlive-base texlive-xetex latex-xcolor texlive-fonts-recommended texlive-latex-extra lmodern texlive-latex-recommended
+  sudo apt-get install -y --no-install-recommeds wget texlive-base texlive-xetex latex-xcolor
+  sudo apt-get install -y --no-install-recommeds texlive-fonts-recommended texlive-latex-extra lmodern texlive-latex-recommended
   sudo apt-get install -y ttf-wqy-microhei ttf-wqy-zenhei
 else
-  docker login -e="DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   sudo add-apt-repository -y ppa:hvr/ghc
   sudo apt-get install -y ghc-$GHCVER cabal-install-$CABALVER
   sudo apt-get install -y ttf-wqy-microhei ttf-wqy-zenhei
   export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
 fi
+echo 'end'
