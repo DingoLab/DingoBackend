c558e10f6924167b1731893161d36da134420b5f
Qinka <qinka@live.com>
2016-05-21 10:48:36 +0800

    update

--- dindo-test/before_install.sh
+++ dindo-test/before_install.sh
@@ -9,13 +9,13 @@ if [ -n "$DOCUMENT" ]; then
   which xetex
 else
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  export Codename=$(lsb_release -a | grep Codename | awk '{print $2}')
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
-  echo 'deb http://download.fpcomplete.com/ubuntu '$Codename' main'|sudo tee /etc/apt/sources.list.d/fpco.list
-  sudo apt-get update && sudo apt-get install stack -y
   sudo add-apt-repository -y ppa:hvr/ghc
   sudo apt-get install -y ghc-$GHCVER cabal-install-$CABALVER
   sudo apt-get install -y ttf-wqy-microhei ttf-wqy-zenhei
   export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
+  export Codename=$(lsb_release -a | grep Codename | awk '{print $2}')
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+  echo 'deb http://download.fpcomplete.com/ubuntu '$Codename' main'|sudo tee /etc/apt/sources.list.d/fpco.list
+  sudo apt-get update && sudo apt-get install stack -y
 fi
 echo 'end'
