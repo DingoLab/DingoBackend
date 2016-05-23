365b18900edbae4ad72b9ead4a91b223766369f1
Qinka <qinka@live.com>
2016-05-23 17:25:59 +0800

    update

--- dindo-test/compiler-test.sh
+++ dindo-test/compiler-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ -n "$STACK" ]; then
-  export STACKFILE=" --stack-yaml $STACK"
+  export STACKFILE=" --stack-yaml $STACK "
 fi
 if [ -n "$LLVM" ]; then
-  export LLVMFLAG=" --ghc-options -fllvm --ghc-options -pgmlo --ghc-options opt-$LLVM --ghc-options -pgmlc --ghc-options llc-$LLVM"
+  export LLVMFLAG=" --ghc-options -fllvm --ghc-options -pgmlo --ghc-options opt-$LLVM --ghc-options -pgmlc --ghc-options llc-$LLVM "
 fi
 if [ -n "$THREADED" ]; then
-  export THREADFLAG=" --ghc-options -threaded"
+  export THREADFLAG=" --ghc-options -threaded "
 fi
-stack install --flag dindo-launch:$BUILDTAGGER $STACKFILE  --ghc-options -O2 $LLVMFLAG $THREADFLAG
+stack install --flag dindo-launch:$BUILDTAGGER $STACKFILE  --ghc-options -O2 $THREADFLAG $LLVMFLAG
