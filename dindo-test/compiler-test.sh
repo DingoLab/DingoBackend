7935133367d836fe883f0ac228f091f088b043c5
Qinka <qinka@live.com>
2016-05-23 05:19:59 +0200

    update

--- dindo-test/compiler-test.sh
+++ dindo-test/compiler-test.sh
@@ -3,6 +3,6 @@ if [ -n "$STACK" ]; then
   export STACKFILE=" --stack-yaml $STACK"
 fi
 if [ -n "$LLVM" ]; then
-  export LLVMFLAG=" --ghc-options -fllvm"
+  export LLVMFLAG=" --ghc-options -fllvm -pgmlo opt-$LLVM -pgmlc llc-$LLVM"
 fi
 stack install --flag dindo-launch:$BUILDTAGGER $STACKFILE  --ghc-options -O2 $LLVMFLAG
