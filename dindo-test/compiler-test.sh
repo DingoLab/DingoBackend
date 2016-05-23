9609ee934b1ec6374ca04ca0a986651368eeab00
Qinka <qinka@live.com>
2016-05-23 17:02:23 +0800

    update

--- dindo-test/compiler-test.sh
+++ dindo-test/compiler-test.sh
@@ -5,7 +5,7 @@ fi
 if [ -n "$LLVM" ]; then
   export LLVMFLAG=" --ghc-options -fllvm --ghc-options -pgmlo --ghc-options opt-$LLVM --ghc-options -pgmlc --ghc-options llc-$LLVM"
 fi
-if [ "$THREADED" == "true" ]; then
+if [ -n "$THREADED" ]; then
   export THREADFLAG=" --ghc-options -threaded"
 fi
 stack install --flag dindo-launch:$BUILDTAGGER $STACKFILE  --ghc-options -O2 $LLVMFLAG $THREADFLAG
