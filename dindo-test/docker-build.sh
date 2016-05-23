97997240216dd3fd128e81ca77cb0c1345b9c25a
Qinka <qinka@live.com>
2016-05-23 20:48:20 +0800

    update

--- dindo-test/docker-build.sh
+++ dindo-test/docker-build.sh
@@ -8,7 +8,7 @@ if [ -n "$LLVM" ]; then
   export DOCKER_IMAGE_TAG=$DOCKER_IMAGE_TAG-llvm-$LLVM
 fi
 if [ -n "$THREADED" ]; then
-  export DOCKER_IMAGR_TAG=threaded-$DOCKER_IMAGE_TAG
+  export DOCKER_IMAGE_TAG=threaded-$DOCKER_IMAGE_TAG
 fi
 export DOCKER_IMAGE_TAG=$TRAVIS_BUILD_NUMBER-$DOCKER_IMAGE_TAG-GHC-$GHCVER-$BUILDTAGGER
 echo build docker
